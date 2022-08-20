6ff8b135ad502c2cb8105ac7a6c1926ceb23e11b
Kader1680 <ouldhenniabaghdad@gmail.com>
2022-08-20 17:09:47 +0100

    code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch} from 'react-router-dom';
 import Home from './home';
 import About from './about';
 import Header from './header';
@@ -18,14 +18,16 @@ function App() {
     <Router>
 
         <Header/>
-
+        <Product/>
         <Routes>
 
-              <Route path='/BABEL/bibal/src/home' element={<Home/>} />
-              
-              <Route path='/BABEL/bibal/src/about' element={<About/>} />
-              <Route path='/BABEL/bibal/src/product' element={<Product/>} />
-              <Route path='/BABEL/bibal/src/contact' element={<Contact/>} /> 
+            <Route path='/BABEL/bibal/src/home' element={<Home/>} />
+
+            <Route path='/BABEL/bibal/src/about' element={<About/>} />
+            <Route path='/BABEL/bibal/src/product' element={<Product/>} />
+            <Route path='/BABEL/bibal/src/contact' element={<Contact/>} /> 
+
+            
 
         </Routes>
         
